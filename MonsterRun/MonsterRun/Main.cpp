497f24739ce6b97f3315ab8c7f22ce0e69ff8c07
Arcaticas <theo.grant9@gmail.com>
2020-09-04 20:35:23 -0600

    final first try

--- MonsterRun/MonsterRun/Main.cpp
+++ MonsterRun/MonsterRun/Main.cpp
@@ -23,10 +23,10 @@ int main()
 
 	int numbMonsters = 0;
 	cout << "How many monsters would you like to run from?\n";
-	while (numbMonsters == 0) {
+	while (numbMonsters <= 0) {
 		gets_s(Input, lenInput);
 		numbMonsters = atoi(Input);
-		if (numbMonsters == 0) {
+		if (numbMonsters <= 0) {
 			cout << "Bad input. Enter an int.\n";
 		}
 	}
@@ -49,8 +49,11 @@ int main()
 	for(int i = 0; i < numbMonsters; i++) 
 	{
 		
-		cout << "What would you like to name monster " << i+1 << "\n";
-		pMonsters[i] = Monster(gets_s(Input, lenInput));
+		cout << "What would you like to name monster " << i+1 << ":\n";
+		if (pMonsters != 0) {
+			pMonsters[i] = Monster(gets_s(Input, lenInput));
+		}
+			
 	}
 
 	
@@ -78,49 +81,61 @@ int main()
 		if (turnCount % 3 == 0) {
 			numbMonsters++;
 			Monster* temp;
+
 			temp = new (nothrow) Monster[numbMonsters];
-			for (int i = 0; i < numbMonsters; i++) {
-				temp[i] = pMonsters[i];
+			if (temp != 0) {
+				for (int i = 0; i < numbMonsters; i++) {
+					if (pMonsters != 0) {
+						temp[i] = pMonsters[i];
+
+					}
+				}
+
+				delete[] pMonsters;
+				cout << "Enter the new monsters name: \n";
+				temp[numbMonsters - 1] = Monster(gets_s(Input, lenInput));
+
+				pMonsters = new (nothrow) Monster[numbMonsters];
+
+				for (int i = 0; i < numbMonsters; i++) {
+					if (pMonsters != 0) 
+					{
+						pMonsters[i] = temp[i];
+					}
+				}
+				delete[] temp;
 			}
-
-			delete[] pMonsters;
-			cout << "Enter the new monsters name: \n";
-			temp[numbMonsters - 1] = Monster(gets_s(Input, lenInput));
-
-			pMonsters = new (nothrow) Monster[numbMonsters];
-
-			for (int i = 0; i < numbMonsters; i++) {
-				pMonsters[i] = temp[i];
-			}
-			delete[] temp;
+			
 		}
-
-		for (int i = 0; i < numbMonsters; i++) 
-		{
-			cout << pMonsters[i].getName() << " is at (" << pMonsters[i].getXPosition() << ',' << pMonsters[i].getYPosition() << ").\n";
-			if (pMonsters[i].getXPosition() == you.getXPosition() && pMonsters[i].getYPosition() == you.getYPosition()) 
+		if (pMonsters != 0) {
+			for (int i = 0; i < numbMonsters; i++)
 			{
-				cout << "A monster got you! Hit any key to quit.";
-				input = _getch();
+				cout << pMonsters[i].getName() << " is at (" << pMonsters[i].getXPosition() << ',' << pMonsters[i].getYPosition() << ").\n";
+				if (pMonsters[i].getXPosition() == you.getXPosition() && pMonsters[i].getYPosition() == you.getYPosition())
+				{
+					cout << "A monster got you! Hit any key to quit.";
+					input = _getch();
+					break;
+				}
+			}
+			cout << "You are at (" << you.getXPosition() << ',' << you.getYPosition() << "). Move now with WASD.\n";
+
+			input = _getch();
+
+			if (input == 27) {
 				break;
 			}
-		}
-		cout << "You are at (" << you.getXPosition() << ',' << you.getYPosition() << "). Move now with WASD.\n";
+			else
+			{
+				you.Move(input);
+			}
 
-		input = _getch();
-		
-		if (input == 27) {
-			break;
-		}
-		else
-		{
-			you.Move(input);
+			for (int i = 0; i < numbMonsters; i++)
+			{
+				pMonsters[i].Move(you);
+			}
 		}
 		
-		for (int i = 0; i < numbMonsters; i++)
-		{
-			pMonsters[i].Move(you);
-		}
 	}
 
 
