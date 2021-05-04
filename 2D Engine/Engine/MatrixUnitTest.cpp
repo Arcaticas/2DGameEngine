f7524addfb2648bb5be09745f50d3ceb4707400f
Arcaticas <theo.grant9@gmail.com>
2021-05-03 22:55:45 -0500

    Vector 4 unit test

--- 2D Engine/Engine/MatrixUnitTest.cpp
+++ 2D Engine/Engine/MatrixUnitTest.cpp
@@ -15,7 +15,9 @@ bool TestMatrix()
 							0, 1, 0, 0,
 							1, 0, 0, 0);
 
-	Vector4 V = Vector4(1, 2, 3, 4);
+	Vector4 C = Vector4(1, 0, 1, 0);
+
+	Vector4 D = Vector4(1, 2, 3, 4);
 
 	assert((A+=B) == Matrix4x4(1, 0, 0, 1,
 							   0, 1, 1, 0,
@@ -32,7 +34,7 @@ bool TestMatrix()
 							   0, 0, 2, 0,
 							   0, 0, 0, 2));
 
-	assert((B*=V) == Vector4(4,3,2,1));
+	assert((B*=D) == Vector4(4,3,2,1));
 
 	assert((A*=B) == Matrix4x4(0, 0, 0, 1,
 							   0, 0, 1, 0,
@@ -49,7 +51,15 @@ bool TestMatrix()
 							   0, 1, 0, 0,
 							   1, 0, 0, 0));
 
-	assert((B * V) == Vector4(4, 3, 2, 1));
+	assert((B * D) == Vector4(4, 3, 2, 1));
+
+	assert((C += D) == Vector4(2, 2, 4, 4));
+
+	assert((D -= C) == Vector4(0, 2, 2, 4));
+
+	assert((D *= 2) == Vector4(2, 4, 6, 8));
+
+	assert((D /= 2) == Vector4(.5f, 1, 1.5f, 2));
 
 
 	return true;
